7512f09ddc723b0d979afa8282fb2350565ea3c9
Poschi <development@poschi.net>
2022-12-03 01:14:55 +0100

    Added Solo 31 and Linking, fixed chart legend

--- main.py
+++ main.py
@@ -8,21 +8,60 @@ st.set_page_config(
     page_icon="🚇",
     layout="wide",
     initial_sidebar_state="expanded",
-    menu_items={
-        'Get Help': 'https://www.extremelycoolapp.com/help',
-        'Report a bug': "https://www.extremelycoolapp.com/bug",
-        'About': "# This is a header. This is an *extremely* cool app!"
-    }
+    # menu_items={
+    #     'Get Help': 'https://www.extremelycoolapp.com/help',
+    #     'Report a bug': "https://www.extremelycoolapp.com/bug",
+    #     'About': "# This is a header. This is an *extremely* cool app!"
+    # }
 )
+
+def param(key, default):
+    params = st.experimental_get_query_params()
+    param = params.get(key)
+    if param:
+        return param[0]
+    else:
+        return default
+
+
 
 with st.sidebar:
     st.header("Eingaben")
-    days = st.slider('Betrachtungszeitraum', 2, 31, 10)
+    days = st.slider(
+        "Betrachtungszeitraum",
+        min_value=2,
+        max_value=31,
+        value=int(param("days", 10))
+    )
+
     st.subheader("Herkömmlich")
-    tarifstufe = st.selectbox("Tarifstufe", options=vgn.TARIFSTUFEN.keys())
-    nbg = st.checkbox("Nürnberg gestreift?", value=True)
+    tarifstufe = st.selectbox(
+        "Tarifstufe",
+        options=vgn.TARIFSTUFEN.keys(),
+        index=list(vgn.TARIFSTUFEN.keys()).index(param("tarifstufe", "A"))
+    )
+
+    nbg = st.checkbox(
+        "Nürnberg gestreift?",
+        value=bool(param("nbg", True))
+    )
+
     st.subheader("Egon")
-    distance = st.number_input("Einfache Distanz in km", value=10.0, step=0.1, min_value=0.1, max_value=200.0, format="%.1f")
+    distance = st.number_input(
+        "Einfache Distanz in km",
+        value=float(param("distance", 10.0)),
+        step=0.1,
+        min_value=0.1,
+        max_value=200.0,
+        format="%.1f"
+    )
+
+    st.experimental_set_query_params(
+        days=days,
+        tarifstufe=tarifstufe,
+        nbg=nbg,
+        distance=distance
+    )
 
 st.header("Kosten")
 st.warning("Status: Experimental", icon="⚠️")
@@ -30,15 +69,17 @@ st.warning("Status: Experimental", icon="⚠️")
 data = []
 for day in range(1, days + 1):
     data.append([
+        day,
         vgn.single_online(day, tarifstufe),
         vgn.day(day, tarifstufe),
+        vgn.TARIFSTUFEN[tarifstufe].solo_31,
         egon.price_for_days(day, distance, nbg)
     ])
 
 chart_data = pd.DataFrame(
     data,
-    columns=['Einzelfahrkarte', 'Tagesticket', 'Egon']
+    columns=["Tag", "Einzelfahrkarte", "Tagesticket", "Solo 31", "Egon"]
 )
 
-st.line_chart(chart_data, height=800)
+st.line_chart(chart_data, height=800,x="Tag",y=["Einzelfahrkarte", "Tagesticket", "Solo 31", "Egon"])
 st.dataframe(chart_data)
--- vgn/__init__.py
+++ vgn/__init__.py
@@ -1,37 +1,38 @@
 class Tarifstufe:
-    def __init__(self, single_online: float, single_offline: float, day: float) -> None:
+    def __init__(self, single_online: float, single_offline: float, day: float, solo_31: float) -> None:
         self.single_online = single_online
         self.single_offline = single_offline
         self.day = day
+        self.solo_31 = solo_31
 
 TARIFSTUFEN = {
-    "A":    Tarifstufe( 2.75,  3.20,  8.30),
-    "B":    Tarifstufe( 2.40,  2.70,  5.70),
-    "C":    Tarifstufe( 2.17,  2.50,  5.10),
-    "D":    Tarifstufe( 1.80,  2.10,  4.70),
-    "E":    Tarifstufe( 1.57,  1.70,  3.70),
-    "F":    Tarifstufe( 1.30,  1.40,  2.90),
-    "K":    Tarifstufe( 1.45,  1.70,  9999), # There is no day ticket for K
+    "A":    Tarifstufe( 2.75,  3.20,  8.30,  80.10),
+    "B":    Tarifstufe( 2.40,  2.70,  5.70,  71.90),
+    "C":    Tarifstufe( 2.17,  2.50,  5.10,  57.70),
+    "D":    Tarifstufe( 1.80,  2.10,  4.70,  46.50),
+    "E":    Tarifstufe( 1.57,  1.70,  3.70,  39.80),
+    "F":    Tarifstufe( 1.30,  1.40,  2.90,  31.80),
+    #"K":    Tarifstufe( 1.45,  1.70,  9999, 9999), # There is no day ticket for K
 
-    "1":    Tarifstufe( 1.80,  2.10,  4.70),
-    "2":    Tarifstufe( 2.51,  2.70,  5.70),
-    "2+T":  Tarifstufe( 3.62,  3.90, 13.60),
-    "3":    Tarifstufe( 3.62,  3.90, 13.60),
-    "3+T":  Tarifstufe( 4.92,  5.30, 13.60),
-    "4":    Tarifstufe( 4.92,  5.30, 13.60),
-    "4+T":  Tarifstufe( 4.92,  5.30, 13.60),
-    "5":    Tarifstufe( 6.04,  6.50, 17.80),
-    "5+T":  Tarifstufe( 6.04,  6.50, 17.80),
-    "6":    Tarifstufe( 7.25,  7.80, 17.80),
-    "6+T":  Tarifstufe( 7.25,  7.80, 17.80),
-    "7":    Tarifstufe( 8.55,  9.20, 17.80),
-    "7+T":  Tarifstufe( 8.55,  9.20, 17.80),
-    "8":    Tarifstufe( 9.67, 10.40, 21.50),
-    "8+T":  Tarifstufe( 9.67, 10.40, 21.50),
-    "9":    Tarifstufe(10.88, 11.70, 21.50),
-    "9+T":  Tarifstufe(10.88, 11.70, 21.50),
-    "10":   Tarifstufe(12.09, 13.00, 21.50),
-    "10+T": Tarifstufe(12.09, 13.00, 21.50)
+    "1":    Tarifstufe( 1.80,  2.10,  4.70,  46.50),
+    "2":    Tarifstufe( 2.51,  2.70,  5.70,  75.90),
+    "2+T":  Tarifstufe( 3.62,  3.90, 13.60,  90.80),
+    "3":    Tarifstufe( 3.62,  3.90, 13.60, 100.90),
+    "3+T":  Tarifstufe( 4.92,  5.30, 13.60, 119.50),
+    "4":    Tarifstufe( 4.92,  5.30, 13.60, 130.70),
+    "4+T":  Tarifstufe( 4.92,  5.30, 13.60, 140.40),
+    "5":    Tarifstufe( 6.04,  6.50, 17.80, 152.60),
+    "5+T":  Tarifstufe( 6.04,  6.50, 17.80, 163.10),
+    "6":    Tarifstufe( 7.25,  7.80, 17.80, 171.00),
+    "6+T":  Tarifstufe( 7.25,  7.80, 17.80, 186.30),
+    "7":    Tarifstufe( 8.55,  9.20, 17.80, 200.00),
+    "7+T":  Tarifstufe( 8.55,  9.20, 17.80, 214.40),
+    "8":    Tarifstufe( 9.67, 10.40, 21.50, 228.20),
+    "8+T":  Tarifstufe( 9.67, 10.40, 21.50, 240.80),
+    "9":    Tarifstufe(10.88, 11.70, 21.50, 254.60),
+    "9+T":  Tarifstufe(10.88, 11.70, 21.50, 266.70),
+    "10":   Tarifstufe(12.09, 13.00, 21.50, 282.10),
+    "10+T": Tarifstufe(12.09, 13.00, 21.50, 303.40)
 }
 
 def single_online(days: float, tarifstufe) -> float:
